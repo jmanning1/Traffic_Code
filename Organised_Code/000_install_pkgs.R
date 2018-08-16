5e1d26ce630af162f3e691f2397788c2ec579ce6
robinlovelace <rob00x@gmail.com>
2018-08-16 11:10:57 +0100

    Update install code, reproducibility

--- Organised_Code/000_install_pkgs.R
+++ Organised_Code/000_install_pkgs.R
@@ -1,6 +1,8 @@
 pkgs_txt = readLines("Organised_Code/000_Load_Libraries.R")
 pkgs = gsub(".*\\((.*)\\).*", "\\1", pkgs_txt)
 nc = nchar(pkgs)
-pkgs = pkgs[nc > 1 & nc < 20]
+pkgs = pkgs[nc > 1 & nc < 20 & !grepl(pattern = "onemin|ukbound", x = pkgs)]
 to_install = !pkgs %in% installed.packages()
 install.packages(pkgs[to_install])
+devtools::install_github("RACFoundation/oneminutetrafficdata")
+devtools::install_github("Robinlovelace/ukboundaries")
